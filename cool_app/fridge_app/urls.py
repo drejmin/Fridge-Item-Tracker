05947a12e9581c6bb5fbb8ca1fe15d1f6966f684
drejmin <djjamn@gmail.com>
2023-08-15 16:57:27 -0700

    modified:   cool_app/cool_app/settings.py 	modified:   cool_app/fridge_app/templates/base.html 	modified:   cool_app/fridge_app/urls.py 	modified:   cool_app/fridge_app/views.py

--- cool_app/fridge_app/urls.py
+++ cool_app/fridge_app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    # path('acounts/signup/', views.signup, name='signup'),
+     path('accounts/signup/', views.signup, name='signup'),
+     path('home/', views.home, name='home')
     # path('items/<int:item_id>/add_photo/', views.add_photo, name='add_photo'),
 ]
 
--- cool_app/fridge_app/views.py
+++ cool_app/fridge_app/views.py
@@ -1,31 +1,30 @@
-# import os
-# import uuid
-# import boto3
-# from django.contrib.auth import login
-# from django.contrib.auth.forms import UserCreationForm
-# from django.contrib.auth.decorators import login_required
-# from django.contrib.auth.mixins import LoginRequiredMixin
-# from django.shortcuts import render, redirect
+import os
+import uuid
+import boto3
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
 
 
-# # Create your views here.
-# def signup(request):
-#   error_message = ''
-#   if request.method == 'POST':
-#     form = UserCreationForm(request.POST)
-#     if form.is_valid():
-#       # Save the user to the db
-#       user = form.save()
-#       # Automatically log in the new user
-#       login(request, user)
-#       return redirect('index')
-#     else:
-#       error_message = 'Invalid sign up - try again'
-#   # A bad POST or a GET request, so render signup template
-#   form = UserCreationForm()
-#   context = {'form': form, 'error_message': error_message}
-#   return render(request, 'registration/signup.html', context)
+# Create your views here.
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
 
+def home(request):
+  return render(request, 'home.html')
 
 # def add_photo(request, item_id):
 #     # photo-file will be the "name" attribute on the <input type="file">
