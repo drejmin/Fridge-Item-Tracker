2f217ec330b97c761945ce2157a2a8ad2f1e413f
drejmin <djjamn@gmail.com>
2023-08-17 16:50:03 -0700

    modified:   cool_app/fridge_app/models.py

--- cool_app/fridge_app/models.py
+++ cool_app/fridge_app/models.py
@@ -6,12 +6,7 @@ from django.urls import reverse
 
 # Perishable Constants
 PERISHABLE_CATEGORIES = (
-    ("B", "Bakery 🍞"),
-    ("D", "Dairy 🥛"),
-    ("E", "Eggs 🐔"),
-    ("M", "Meat and Seafood 🍖"),
-    ("P", "Produce 🥦"),
-    ("O", "Other 🍕"),
+
     ("B", "Bakery 🍞"),
     ("D", "Dairy 🥛"),
     ("E", "Eggs 🐔"),
@@ -29,14 +24,6 @@ PERISHABLE_CATEGORIES_EMOJIS = {
     'O': '🍕',
 }
 
-PERISHABLE_CATEGORIES_EMOJIS = {
-    'B': '🍞',
-    'D': '🥛',
-    'E': '🐔',
-    'M': '🍖',
-    'P': '🥦',
-    'O': '🍕',
-}
 
 
 # Reminder Constants
@@ -61,8 +48,6 @@ TIME_ZONES = (
     ("ET", "Eastern Time"),
 )
 
-
-
 class Receipt(models.Model):
     store_name= models.CharField(max_length=30)
     purchase_date=models.DateField('Purchase Date')
@@ -73,9 +58,15 @@ class Receipt(models.Model):
         decimal_places=2,
         validators=[MinValueValidator(0)],
         )
+    receipt_image=models.ImageField('Image', blank = True)
     item_list = models.TextField(max_length=1000)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"Photo for receipt_id: {self.receipt_id} @{self.url}"
+    
 
     class Meta:
         ordering = ['-purchase_date']
@@ -86,9 +77,6 @@ class Receipt(models.Model):
     def get_absolute_url(self):
         return reverse('receipt_detail', kwargs={'receipt_id': self.id})
     
-class Photo(models.Model):
-        url = models.CharField(max_length=200)
-        receipt = models.ForeignKey(Receipt, on_delete=models.CASCADE, null=True, blank=True)
 
         def __str__(self):
             return f"Photo for receipt_id: {self.receipt_id} @{self.url}"
@@ -160,11 +148,7 @@ class Perishable(models.Model):
         return reverse('perishables_detail', kwargs={'pk': self.id})
     
     def __str__(self):
-        return f'{self.name} ({self.id})'        
-
-    def get_emoji(self):
-        return PERISHABLE_CATEGORIES_EMOJIS[self.category]
-
-
+        return f'{self.name} ({self.id})'
+    
     def get_emoji(self):
         return PERISHABLE_CATEGORIES_EMOJIS[self.category]
