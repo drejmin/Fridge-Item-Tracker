ad720300afd3fbc0b2b5cdbc24472055fd3b46d3
LeepDev <hyungy@gmail.com>
2023-08-17 19:04:29 -0500

    fixed missing static files in settings

--- cool_app/cool_app/settings.py
+++ cool_app/cool_app/settings.py
@@ -125,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'firdge_app/static')]
+
 LOGIN_REDIRECT_URL = '/home/'
 LOGOUT_REDIRECT_URL = '/'
 # Default primary key field type
