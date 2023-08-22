1b8c59f4e86e196ef81b0563bfdae80c7c19c814
drejmin <djjamn@gmail.com>
2023-08-22 14:07:50 -0700

    new file:   cool_app/.gitignore 	new file:   cool_app/Procfile 	modified:   cool_app/cool_app/settings.py 	modified:   cool_app/fridge_app/templates/home.html 	new file:   cool_app/requirements.txt

--- cool_app/cool_app/settings.py
+++ cool_app/cool_app/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # add code below
 import environ
 import os
+import dj_database_url
+PRODUCTION = 'PRODUCTION' in os.environ
 
 
 environ.Env()
@@ -31,16 +33,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-k1m8(u-0gx#)e+6lnbi$-mr+m9+0a+3l9&n+a23$h0%gwsdnb!'
-
+if PRODUCTION:
+    SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+if PRODUCTION:
+    DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+     'whitenoise.runserver_nostatic',
     'fridge_app',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -58,6 +64,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'cool_app.urls'
@@ -90,7 +98,9 @@ DATABASES = {
         'NAME': 'fridge',
     }
 }
-
+if PRODUCTION:
+    db_from_env = dj_database_url.config(conn_max_age=600)
+    DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -127,6 +137,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = str(BASE_DIR) + "/staticfiles"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'fridge_app/static')]
 
 LOGIN_REDIRECT_URL = '/home/'
